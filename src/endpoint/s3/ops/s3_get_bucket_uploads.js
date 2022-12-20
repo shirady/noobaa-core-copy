5dc9dfd96934228871e08233fc3cbc9eb277bb72
shirady <57721533+shirady@users.noreply.github.com>
2022-12-20 12:04:55 +0200

    add encoding changes to multipart uploads

--- src/endpoint/s3/ops/s3_get_bucket_uploads.js
+++ src/endpoint/s3/ops/s3_get_bucket_uploads.js
@@ -26,34 +26,38 @@ async function get_bucket_uploads(req) {
         limit: Math.min(max_keys_received, 1000),
     });
 
+    const encoding_type = req.query['encoding-type'];
+    const encoding_flag = s3_utils.check_encoding_type(encoding_type);
+    const encode_field = encoding_flag ? s3_utils.encode_url_field : s3_utils.no_encode;
+
     return {
         ListMultipartUploadsResult: [{
-                'Bucket': req.params.bucket,
-                'Prefix': req.query.prefix,
-                'Delimiter': req.query.delimiter,
-                'MaxUploads': max_keys_received,
-                'KeyMarker': req.query['key-marker'],
-                'UploadIdMarker': req.query['upload-id-marker'],
-                'IsTruncated': reply.is_truncated,
-                'NextKeyMarker': reply.next_marker,
-                'NextUploadIdMarker': reply.next_upload_id_marker,
-                'Encoding-Type': req.query['encoding-type'],
-            },
-            _.map(reply.objects, obj => ({
-                Upload: {
-                    Key: obj.key,
-                    UploadId: obj.obj_id,
-                    Initiated: s3_utils.format_s3_xml_date(obj.upload_started),
-                    Initiator: s3_utils.DEFAULT_S3_USER,
-                    Owner: s3_utils.DEFAULT_S3_USER,
-                    StorageClass: s3_utils.STORAGE_CLASS_STANDARD,
-                }
-            })),
-            _.map(reply.common_prefixes, prefix => ({
-                CommonPrefixes: {
-                    Prefix: prefix || ''
-                }
-            }))
+            'Bucket': req.params.bucket,
+            'Prefix': encode_field(req.query.prefix),
+            'Delimiter': encode_field(req.query.delimiter),
+            'MaxUploads': max_keys_received,
+            'KeyMarker': encode_field(req.query['key-marker']),
+            'UploadIdMarker': req.query['upload-id-marker'],
+            'IsTruncated': reply.is_truncated,
+            'NextKeyMarker': encode_field(reply.next_marker),
+            'NextUploadIdMarker': reply.next_upload_id_marker,
+            'EncodingType': encoding_type,
+        },
+        _.map(reply.objects, obj => ({
+            Upload: {
+                Key: encode_field(obj.key),
+                UploadId: obj.obj_id,
+                Initiated: s3_utils.format_s3_xml_date(obj.upload_started),
+                Initiator: s3_utils.DEFAULT_S3_USER,
+                Owner: s3_utils.DEFAULT_S3_USER,
+                StorageClass: s3_utils.STORAGE_CLASS_STANDARD,
+            }
+        })),
+        _.map(reply.common_prefixes, prefix => ({
+            CommonPrefixes: {
+                Prefix: encode_field(prefix) || ''
+            }
+        }))
         ]
     };
 }
